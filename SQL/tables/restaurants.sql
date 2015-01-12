4986f42562e257e2e633256152fffd6a0896be52
Sam Blinstein <sam.blinstein@gmail.com>
2015-01-11 16:55:37 -0800

    added restaurants table sql file

---
+++ SQL/tables/restaurants.sql
@@ -0,0 +1,13 @@
+﻿-- Restaurant Table
+
+CREATE TABLE RESTAURANTS
+(
+id serial primary key, --auto-incrementing primary key for each row
+name varchar(255),
+longitude decimal(11,8),
+latitude decimal(11,8),
+address varchar(255), --street address
+rating decimal(2,1), --yelp rating
+price_range smallint CHECK (price_range > 0), --number of yelp dollar signs
+url varchar(255)
+);
